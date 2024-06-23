f939ecf0fefca963a64ec16b48668042174bcce6
iwopant0825 <ccodingpy0825@gmail.com>
2024-06-23 18:43:13 +0900

    like dog scroll

--- app/page.jsx
+++ app/page.jsx
@@ -8,33 +8,35 @@ import {
   useScroll,
 } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 export default function Home() {
   return (
     <>
-      <Canvas style={{ position: "absolute"}}>
+      <Canvas style={{ position: "absolute" }}>
         <ambientLight />
-        <ScrollControls pages={3}>
-           <Scene /> 
-          <Scroll html>
-            <Over/>
+        <ScrollControls pages={4} damping={0.2}>
+          <ScrollManager />
+          <Scene />
+          <Scroll html style={{ width: "100%" }}>
+            <Over />
           </Scroll>
         </ScrollControls>
       </Canvas>
     </>
   );
 }
+
+function ScrollManager() {
+  const data = useScroll();
+  data.fill.classList.add("top-0");
+  data.fill.classList.add("absolute");
+
+  return null;
+}
+
 function Scene() {
-  const ref = useRef();
-  const scroll = useScroll();
-  const scrolls = useScroll().offset * 3 + 1;
-
-  // useFrame(() => {
-  //   console.log(scroll.offset * 3 + 1);
-  // });
-
   return (
     <mesh>
       <Box args={[1, 1, 1]} />
@@ -44,12 +46,55 @@ function Scene() {
 }
 
 function Over() {
+  const scroll = useScroll();
+  const [scrollData, setScrollData] = useState(scroll.offset * 3 + 1);
+  const test1 = useRef(null);
+  const test2 = useRef(null);
+  const test3 = useRef(null);
+  const test4 = useRef(null);
+
+  const scrollToSection = (elementRef) => {
+    scroll.el.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    });
+  };
+
+  useFrame(() => {
+    setScrollData(scroll.offset * 3 + 1); // 현재 스크롤 오프셋을 콘솔에 출력
+  });
+
+  useEffect(() => {
+    if (scrollData >= 1.01 && scrollData < 1.1) {
+      scrollToSection(test2);
+    } 
+    
+    else if (scrollData >= 2.01 && scrollData < 2.1) {
+      scrollToSection(test3);
+    } 
+    
+    else if (scrollData >= 3.01 && scrollData < 3.1) {
+      scrollToSection(test4);
+    }
+
+    console.log(scrollData);
+  }, [scrollData]);
+
   return (
-    <div>
-            <div style={{ height: "100vh" }}>Page 1</div>
-      <div style={{ height: "100vh" }}>Page 2</div>
-      <div style={{ height: "100vh" }}>Page 3</div>
-    </div>
+    <>
+      <div ref={test1} style={{ height: "100vh", width: "100%" }}>
+        Page 1
+      </div>
+      <div ref={test2} style={{ height: "100vh", width: "100%" }}>
+        Page 2
+      </div>
+      <div ref={test3} style={{ height: "100vh", width: "100%" }}>
+        Page 3
+      </div>
+      <div ref={test4} style={{ height: "100vh", width: "100%" }}>
+        Page 4
+      </div>
+    </>
   );
 }
 
