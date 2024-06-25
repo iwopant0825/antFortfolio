4b09d57438796758d7e272284cf157e378355ceb
iwopant0825 <129718228+iwopant0825@users.noreply.github.com>
2024-06-26 00:17:12 +0900

    re

--- app/page.jsx
+++ app/page.jsx
@@ -1,127 +1,32 @@
 "use client";
 
 import {
-  Box,
   OrbitControls,
   Scroll,
   ScrollControls,
-  useScroll,
 } from "@react-three/drei";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import ScrollManager from "./components/ScrollManager";
+import Scene from "./components/Scene";
+import Over from "./components/Over";
+import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
 
 export default function Home() {
+  const [sceneAn, setSceneAn] = useState(1)
   return (
     <Layout>
       <Canvas style={{ position: "absolute" }}>
         <ambientLight />
-        <ScrollControls pages={4} damping={0.2}>
+        <ScrollControls pages={5} damping={0.2}>
           <ScrollManager />
-          <Scene />
+          <Scene sceneAn={sceneAn}/>
           <Scroll html style={{ width: "100%" }}>
-            <Over />
+            <Over setSceneAn={setSceneAn}/>
           </Scroll>
         </ScrollControls>
       </Canvas>
     </Layout>
-  );
-}
-
-function ScrollManager() {
-  const data = useScroll();
-  data.fill.classList.add("top-0");
-  data.fill.classList.add("absolute");
-
-  return null;
-}
-
-function Scene() {
-  return (
-    <mesh>
-      <Box args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  );
-}
-
-function Over() {
-  const scroll = useScroll();
-  const test1 = useRef(null);
-  const test2 = useRef(null);
-  const test3 = useRef(null);
-  const test4 = useRef(null);
-  const [page, setpage] = useState(1);
-
-  const scrollToSection = (elementRef) => {
-    scroll.el.scrollTo({
-      top: elementRef.current.offsetTop,
-      behavior: "auto",
-    });
-  };
-
-  useFrame(() => {
-    const scrollData = scroll.offset * 3 + 1;
-
-    if (page == 1) {
-      if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
-        scrollToSection(test2);
-      }
-    } else if (page == 2) {
-      if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
-        scrollToSection(test3);
-      } else if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
-        scrollToSection(test1);
-      }
-    } else if (page == 3) {
-      if (scrollData >= page + 0.0001 && scrollData < page + 0.8) {
-        scrollToSection(test4);
-      } else if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
-        scrollToSection(test2);
-      }
-    } else if (page == 4) {
-      if (scrollData <= page - 0.0001 && scrollData > page - 0.8) {
-        scrollToSection(test3);
-      }
-    }
-
-    if (scrollData == 1) {
-      setpage(1);
-    } else if (scrollData == 2) {
-      setpage(2);
-    } else if (scrollData == 3) {
-      setpage(3);
-    } else if (scrollData == 4) {
-      setpage(4);
-    }
-
-    if (
-      scrollData !== 1 &&
-      scrollData !== 2 &&
-      scrollData !== 3 &&
-      scrollData !== 4
-    ) {
-      scroll.el.style.overflow = "hidden";
-    } else {
-      scroll.el.style.overflow = "auto";
-    }
-  });
-
-  return (
-    <>
-      <div ref={test1} style={{ height: "100vh", width: "100%" }}>
-        Page 1
-      </div>
-      <div ref={test2} style={{ height: "100vh", width: "100%" }}>
-        Page 2
-      </div>
-      <div ref={test3} style={{ height: "100vh", width: "100%" }}>
-        Page 3
-      </div>
-      <div ref={test4} style={{ height: "100vh", width: "100%" }}>
-        Page 4
-      </div>
-    </>
   );
 }
 
